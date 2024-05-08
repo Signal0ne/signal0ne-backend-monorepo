70d98f087ae021280c71ae5bc7f9fdd4b4c86599
SzymonSt <szymonst2808@gmail.com>
2024-05-08 19:35:31 +0200

    feat: code_gen code matching issue context verification

--- solutionAgent/agents/code_gen.py
+++ solutionAgent/agents/code_gen.py
@@ -22,18 +22,22 @@ class CodeGen:
     def gen_code(self, logs, current_code, predicted_solutions, languageId):
         """Generate code snippets from the logs."""
 
-        prompt = f"""You are a helpful assistant that helps to fix code written in {languageId}. 
-        You return a json with the code snippet. You only return the code no explanation.
-        Your return type is json newline must be \\n.
-        Here are the logs: {logs} caused by issue in code fix it\n
-        Here is the broken code: {current_code}\n
-        Here is additional context on the issue and proposed solutions by other engineer: {predicted_solutions}\n
-        Your output format is {{"code":"your code snippet"}}. Json:"""
-        result = self.__execute(prompt)
-        result = parse_json(result)
-        result_object = json.loads(result)
-        result = self.__base_reflection(result_object["code"], languageId)
-        return json.loads(result)
+        initial_reflection = self.__base_code_macthing_issue_reflection(current_code, logs, languageId)
+        if initial_reflection["relevant"] == "true":
+            prompt = f"""You are a helpful assistant that helps to fix code written in {languageId}. 
+            You return a json with the code snippet. You only return the code no explanation.
+            Your return type is json newline must be \\n.
+            Here are the logs: {logs} caused by issue in code fix it\n
+            Here is the broken code: {current_code}\n
+            Here is additional context on the issue and proposed solutions by other engineer: {predicted_solutions}\n
+            Your output format is {{"code":"your code snippet"}}. Json:"""
+            result = self.__execute(prompt)
+            result = parse_json(result)
+            result_object = json.loads(result)
+            result = self.__base_reflection(result_object["code"], languageId)
+            return json.loads(result)
+        else:
+            return {"code": ""}
     
     def __base_reflection(self, initial_result, languageId):
         """Reflect on the result as Code Reviewer persona"""
@@ -44,6 +48,19 @@ class CodeGen:
         Your return type is json newline must be \\n.
         Here is the code to be reviewed: {initial_result}\n
         Your output format is {{"code":"your code snippet with your changes"}}. Json:"""
+        result = self.__execute(prompt)
+        result = parse_json(result)
+        return result
+    
+    def __base_code_macthing_issue_reflection(self, current_code, logs, languageId):
+        """Reflect if the code matches the issue context as Software Engineer persona"""
+        
+        prompt = f"""Your are helpful assistant who helps estimate if the code can be fixed based on provided logs 
+        You return json with true or false. True if the code can be fixed with the issue context and false otherwise.
+        Code: {current_code}\n
+        Language: {languageId}\n
+        Logs: {logs}\n
+        Your output format is {{"relevant":"true or false value"}}. Json:"""
         result = self.__execute(prompt)
         result = parse_json(result)
         return result
