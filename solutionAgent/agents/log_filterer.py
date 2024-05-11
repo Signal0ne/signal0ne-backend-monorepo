287f7f6521001eb5533e8cf2429693e09814585e
SzymonSt <szymonst2808@gmail.com>
2024-05-11 23:02:42 +0200

    improved log filtering prompt

--- solutionAgent/agents/log_filterer.py
+++ solutionAgent/agents/log_filterer.py
@@ -15,27 +15,27 @@ class LogFilterer:
             self.llm = ChatOpenAI(
                 api_key=os.getenv("OPENAI_API_KEY"),
                 model=endpoint,
-                temperature=0.4,
+                temperature=0.3,
                 max_tokens=512,
-                frequency_penalty=1.1
+                frequency_penalty=1.3
             )
-            self.prompt = """System: You are a helpful software engineer whose job is to filter the relevant and most specific logs from logtail.
-                            Filter the logs given below to only the most relevant and specific logs. Do not include any irrelevant logs.
+            self.prompt = """System: You are a helpful software engineer whose job is to filter the relevant logs from logtail to resolve the issue in the logs.
+                            Filter the logs given below to only the most relevant logs that directly indicate issue. Do not include any irrelevant logs.
                             Logs: {logs}"""
         else:
             self.llm = OpenAI(
                     api_key=os.getenv("OPENAI_API_KEY"),
                     name=endpoint,
-                    temperature=0.4,
+                    temperature=0.3,
                     max_tokens=512,
-                    frequency_penalty=1.1
+                    frequency_penalty=1.3
                 )
-            self.prompt = """System: You are a helpful software engineer whose job is to filter the relevant and most specific logs from logtail.
-                            Filter the logs given below to only the most relevant and specific logs. Do not include any irrelevant logs.
+            self.prompt = """System: You are a helpful software engineer whose job is to filter the relevant logs from logtail to resolve the issue in the logs.
+                            Filter the logs given below to only the most relevant logs that directly indicate issue. Do not include any irrelevant logs.
                             Logs: {logs}"""
         
-    def filter_relevant_logs(self, logs):
-        filter_prompt = self.prompt.format(logs=logs)
+    def filter_relevant_logs(self, logs, severity="INFO"):
+        filter_prompt = self.prompt.format(logs=logs, severity=severity)
         return self.__execute(filter_prompt)
 
     def __execute(self, formatted_prompt: str):
